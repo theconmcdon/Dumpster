4f1223b042eff6dbe2de1e8a9f31548a10c7a4ef
theconmcdon <83612766+theconmcdon@users.noreply.github.com>
2021-07-27 10:17:27 -0500

    more minor changes

--- src/client/components/Detail.tsx
+++ src/client/components/Detail.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 
 export interface DetailProps extends RouteComponentProps<{ id: string; }> { }
 
-const Detail = ({history, match: { params: { id }}}) => {
+const Detail = ({ history, match: { params: { id } } }) => {
 
     const imgBuddy = {
         height: '70px',
@@ -35,41 +35,97 @@ const Detail = ({history, match: { params: { id }}}) => {
     }, [id]);
 
 
-    return(
+    const hoverEnterLike = () => {
+        let likeBtn = document.querySelector('#btnLike')
+        likeBtn.classList.add('text-danger')
+    }
+
+    const hoverLeaveLike = () => {
+        let likeBtn = document.querySelector('#btnLike')
+        likeBtn.classList.remove('text-danger')
+    }
+
+    const hoverEnterRec = () => {
+        let postBtn = document.querySelector('#btnRec')
+        postBtn.classList.add('text-danger')
+    }
+
+    const hoverLeaveRec = () => {
+        let postBtn = document.querySelector('#btnRec')
+        postBtn.classList.remove('text-danger')
+    }
+
+    const hoverEnterSave = () => {
+        let postBtn = document.querySelector('#btnSave')
+        postBtn.classList.add('text-danger')
+    }
+
+    const hoverLeaveSave = () => {
+        let postBtn = document.querySelector('#btnSave')
+        postBtn.classList.remove('text-danger')
+    }
+
+    const hoverEnterShare = () => {
+        let postBtn = document.querySelector('#btnShare')
+        postBtn.classList.add('text-danger')
+    }
+
+    const hoverLeaveShare = () => {
+        let postBtn = document.querySelector('#btnShare')
+        postBtn.classList.remove('text-danger')
+    }
+
+    const hoverBuddy = {
+        cursor: 'pointer'
+    }
+
+    return (
         <section>
-            <article className="col-md-12"> 
-                <div className="card m-1 p-1 shadow">
-                {array.map(val => {
-                    return (
-                        
+            <div className='mt-5 row'>
+                <div className='col-1'></div>
+                <div id='test' className='col-1 beep align-self-center lead'>
+                    <div>
+                        <div id='btnLike' style={hoverBuddy} onMouseEnter={() => hoverEnterLike()} onMouseLeave={() => hoverLeaveLike()} className='border-right mt-3 lead border-secondary text-right pr-2'>like</div>
+                    </div>
+                    <div className=''>
+                        <div id='btnRec' style={hoverBuddy} onMouseEnter={() => hoverEnterRec()} onMouseLeave={() => hoverLeaveRec()} className='border-right lead border-secondary text-right pr-2'>repost</div>
+                    </div>
+                    <div>
+                        <div id='btnSave' style={hoverBuddy} onMouseEnter={() => hoverEnterSave()} onMouseLeave={() => hoverLeaveSave()} className='border-right lead border-secondary text-right pr-2'>save</div>
+                    </div>
+                    <div>
+                        <div id='btnShare' style={hoverBuddy} onMouseEnter={() => hoverEnterShare()} onMouseLeave={() => hoverLeaveShare()} className='border-right mb-3 lead border-secondary text-right pr-2'>share</div>
+                    </div>
+                </div>
+                <div className="col-md-9">
+
+                    {array.map(val => {
+                        return (
+
                             <blockquote key={id} style={chirpBuddy} className="blockquote bg-white border p-5">
                                 <div className='row'>
                                     <img style={imgBuddy} src="https://pbs.twimg.com/media/C8QqGm4UQAAUiET.jpg" alt="" />
                                     <div className='col-8'>
                                         <div className='pl-5 lead'>(nickname)</div>
-                                        <p  className="pl-5 mb-0">{val.text}</p>
+                                        <p className="pl-5 mb-0">{val.text}</p>
                                         <footer className="ml-5 blockquote-footer">@username on <cite title="Source Title">{val.day} at {val.time}</cite></footer>
                                     </div>
                                 </div>
                             </blockquote>
-                        
-                    )
-                })}
-                    <div className="card-body text-center">
-                        <h4 className="lead card-title"></h4>
-                        <div className="d-flex justify-content-center align-items-center mb-2">
-                            <p className="card-text m-3"></p>
-                            
-                        </div>
-                        
-                        <button onClick={() => history.goBack()} className='btn btn-danger mx-auto'>go back</button>
-                    </div>
+
+                        )
+                    })}
+
+
                 </div>
-            </article>
+                <div className='col-1'></div>
+            </div>
+            <div className="row card-body text-center">
+                <button onClick={() => history.goBack()} className='btn btn-danger mx-auto'>go back</button>
+            </div>
         </section>
     )
 }
 
 export default Detail;
 
-    
